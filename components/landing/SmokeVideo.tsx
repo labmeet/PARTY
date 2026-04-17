95a2e0b0d48f6614f008beff7b8a7de8d72fc450
Minsuk Jang(Jaden) <160453556+tersite1@users.noreply.github.com>
2026-04-18 01:32:31 +0900

    Update SmokeVideo.tsx

--- components/landing/SmokeVideo.tsx
+++ components/landing/SmokeVideo.tsx
@@ -2,14 +2,17 @@
 
 export function SmokeVideo() {
   return (
-    <div className="relative w-full h-[50vh] overflow-hidden">
+    <div
+      className="relative w-full h-[50vh] overflow-hidden"
+      style={{ mixBlendMode: 'screen' }}
+    >
       <video
         autoPlay
         loop
         muted
         playsInline
+        ref={(el) => { if (el) el.playbackRate = 0.5; }}
         className="absolute bottom-0 left-0 w-full h-full object-cover"
-        style={{ mixBlendMode: 'screen' }}
       >
         <source src="/smoke.webm" type="video/webm" />
       </video>
